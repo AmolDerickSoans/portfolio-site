b6bd36c2574d94545f942a7a43d52ed920e766b2
Amol Derick Soans <amoldericksoans@gmail.com>
2020-08-06 17:18:05 +0530

    rev  1 not many changes

---
+++ gatsby-config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  siteMetadata: {
+    title: `Amol Soans | gatsby based protfolio and PWA`,
+        description: `A starter powered by Gatsby and Prismic to showcase portfolios and blogs.`,
+    author: `Marguerite Roth | marguerite.io`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-image`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-resolve-src`,
+    `gatsby-plugin-remove-trailing-slashes`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+        resolve: '@prismicio/gatsby-source-prismic-graphql',
+        options: {
+            repositoryName: 'amol', // (REQUIRED, replace with your own)
+            linkResolver: () => post => `/${post.uid}`,
+        }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-prismic-starter-prist`,
+        short_name: `prist`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/oscar-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
+    {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+            trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+            head: true,
+        },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-netlify-cache',
+  ],
+}
---
+++ src/styles/dimensions.js
@@ -0,0 +1,14 @@
+//Dimensions for device media queries and layout padding
+const dimensions = {
+    //pixels
+    maxwidthDesktop: 1100,
+    maxwidthTablet: 768,
+    maxwidthMobile: 600,
+
+    //ems
+    paddingHorizontalDesktop: 1,
+    paddingHorizontalTablet: 1,
+    paddingHorizontalMobile: 2,
+}
+
+export default dimensions;
